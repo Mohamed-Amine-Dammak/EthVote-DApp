1fb21093f1c8b5ff2fcf70b7e709d9308cfa5341
Mohamed-Amine-Dammak <95967020+Mohamed-Amine-Dammak@users.noreply.github.com>
2025-09-27 17:58:30 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VotingApp from './VotingApp';
+
+function App() {
+  return (
+    <div className="App">
+      <VotingApp />
+    </div>
+  );
+}
+
+export default App;
